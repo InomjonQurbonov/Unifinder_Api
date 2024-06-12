39bec2710d364d3d2f4e3fc62319a37bc3fedb78
Inomjon Qurbonov <145829813+InomjonQurbonov@users.noreply.github.com>
2024-06-12 19:45:44 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['InomjonQurbonov.pythonanywhere.com']
 
 
 # Application definition
